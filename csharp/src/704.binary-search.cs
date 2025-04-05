9477ddddd94114c3da2541850dc53d02f52c0107
yashsachdev <yashsachdev5677@gmail.com>
2025-04-05 23:53:57 +0530

    binary search

---
+++ csharp/src/704.binary-search.cs
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=704 lang=csharp
+ *
+ * [704] Binary Search
+ *
+ * https://leetcode.com/problems/binary-search/description/
+ *
+ * algorithms
+ * Easy (59.27%)
+ * Likes:    12469
+ * Dislikes: 271
+ * Total Accepted:    3.1M
+ * Total Submissions: 5.2M
+ * Testcase Example:  '[-1,0,3,5,9,12]\n9'
+ *
+ * Given an array of integers nums which is sorted in ascending order, and an
+ * integer target, write a function to search target in nums. If target exists,
+ * then return its index. Otherwise, return -1.
+ * 
+ * You must write an algorithm with O(log n) runtime complexity.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [-1,0,3,5,9,12], target = 9
+ * Output: 4
+ * Explanation: 9 exists in nums and its index is 4
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [-1,0,3,5,9,12], target = 2
+ * Output: -1
+ * Explanation: 2 does not exist in nums so return -1
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 10^4
+ * -10^4 < nums[i], target < 10^4
+ * All the integers in nums are unique.
+ * nums is sorted in ascending order.
+ * 
+ * 
+ */
+
+// @lc code=start
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.Length-1;
+        while(low<=high)
+        {
+            int mid = low+((high-low)/2);
+            if(nums[mid] > target)
+            {
+                high = mid-1;
+            }
+            else if (nums[mid] < target){
+                low = mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
+// @lc code=end
+
